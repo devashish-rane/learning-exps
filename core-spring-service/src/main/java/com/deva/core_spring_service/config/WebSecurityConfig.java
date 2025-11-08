9ca329c907361ab78f9a2c47d1b64e7fd679908a
Devashish Rane <ranedevashish2@gmail.com>
2025-11-08 12:41:13 +0530

    Refactor security configuration to permit all requests and remove commented-out code; clean up docker-compose by removing unused database service

--- core-spring-service/src/main/java/com/deva/core_spring_service/config/WebSecurityConfig.java
+++ core-spring-service/src/main/java/com/deva/core_spring_service/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.deva.core_spring_service.config;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -14,11 +15,19 @@ public class WebSecurityConfig {
         http
                 .csrf(csrf -> csrf.disable()) // for APIs
                 .headers(headers -> headers.frameOptions().disable())
+                // .authorizeHttpRequests(auth -> auth
+                //         // Permit health and info via Actuator matcher (most reliable)
+                //         .requestMatchers(EndpointRequest.to("health", "info")).permitAll()
+                //         // Permit API routes
+                //         .requestMatchers("/api/**").permitAll()
+                //         // H2 console in dev (optional)
+                //         .requestMatchers("/h2-console/**").permitAll()
+                //         .anyRequest().authenticated()
+                // )
+                // .httpBasic(Customizer.withDefaults()); // username/password popup
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/**", "/actuator/health", "/actuator/info").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults()); // username/password popup
+                        .anyRequest().permitAll()
+                );
         return http.build();
     }
 }
